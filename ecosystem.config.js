117dbc4f95354dca8f89616167477d6e8ac849b3
dtpietrzak <dtpietrzak@gmail.com>
2023-12-04 19:17:26 -0600

    pm22

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   apps: [{
     name: "ez-spend",
-    script: "npm",
-    args: "run prod"
-  }]
-}
+    script: "./start.sh",
+    exec_mode: "fork",
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: "production",
+    },
+  }],
+};
--- script.sh
+++
@@ -1,1 +0,0 @@
-sudo su -c "env PATH=$PATH:/home/unitech/.nvm/versions/node/v14.3/bin pm2 startup <distribution> -u <user> --hp <home-path>
