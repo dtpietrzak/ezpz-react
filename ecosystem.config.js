d3ced38cece3304c6ef01edece13fc9d2f483120
dtpietrzak <dtpietrzak@gmail.com>
2023-12-04 19:10:22 -0600

    pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  apps: [{
+    name: "ez-spend",
+    script: "npm",
+    args: "run prod"
+  }]
+}
---
+++ script.sh
@@ -0,0 +1,1 @@
+sudo su -c "env PATH=$PATH:/home/unitech/.nvm/versions/node/v14.3/bin pm2 startup <distribution> -u <user> --hp <home-path>
